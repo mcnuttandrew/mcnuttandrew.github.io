c334bb3af92348cb871b83178ef5dce1ca8fa9cf
Andrew Mc Nutt <mcnutt@uber.com>
2016-05-16 02:24:38 -0400

    first page up and styled

---
+++ app/client/components/app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var FirstPageContent = require('./first-page-content.jsx');
+
+module.exports = React.createClass({
+  displayName : 'App',
+  render() {
+    return <FirstPageContent />
+  }
+});
---
+++ app/client/components/first-page-content.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var FirstPageContent = React.createClass({
+  displayName : 'FirstPageContent',
+  render() {
+    return (
+      <div className="app">
+        <div className="container">
+          <div className="tagline">
+            <div>SF BASED DATA</div>
+            <div>VISUALIZATION</div>
+            <div className="special-line">EXPERT & APPLIED</div>
+            <div>MATHER</div>
+          </div>
+          <div className="name"> ANDREW MCNUTT</div>
+          <div className="boring-content">I am an applied mathematician currently working in the wild world of San Francisco based web development. I am a full stack developer, but I am more or less front-end/data-visualization leaning. My educational background is in physics, specifically in mechaincs, and traditional rails oriented web development. I feel greatly for the plight of the American desert.</div>
+          <div className="links">
+            <div><a className="link" href='/work'>WORK</a></div>
+            <div><a className="link" href='/work'>RESEARCH</a></div>
+            <div><a className="link" href='/work'>CV</a></div>
+            <div><a className="link" href='/work'>CONTACT</a></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+
+module.exports = FirstPageContent;
