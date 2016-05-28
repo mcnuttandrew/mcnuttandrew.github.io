9635470079ee15c20f0765bd77a1563c867b7a81
Andrew Mc Nutt <mcnutt@uber.com>
2016-05-28 16:15:47 -0700

    compile error

--- app/client/app.js
+++ app/client/app.js
@@ -3,20 +3,11 @@ var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var IndexRoute = ReactRouter.IndexRoute;
 
 var App = require('./components/app.jsx');
-var FirstPageContent = require('./components/first-page-content.jsx');
-var ResearchPage = require('./components/research-page.jsx');
-var WorkPage = require('./components/work-page.jsx');
 
 ReactDOM.render((
   <Router history={ReactRouter.browserHistory}>
-    <Route path="/" component={App}>
-    </Route>
+    <Route path="/" component={App}></Route>
   </Router>
 ),  document.getElementById('app'))
-// <IndexRoute component={FirstPageContent}> </IndexRoute>
-// <Route path="/about" component={FirstPageContent}> </Route>
-// <Route path="/research" component={ResearchPage}> </Route>
-// <Route path="/work" component={WorkPage}> </Route>
