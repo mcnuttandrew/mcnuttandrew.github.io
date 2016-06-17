e5c3f3acb3113a6afb248d8c70f823f9d360a6eb
Andrew Mc Nutt <mcnutt@uber.com>
2016-06-16 23:34:36 -0700

    Add GA tracking

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGa from 'react-ga';
 import {browserHistory, Router, Route} from 'react-router';
 
 import Root from './components/root.jsx';
 
+ReactGa.initialize('UA-79506537-1');
+
+function logPageView() {
+  ReactGa.pageview(window.location.pathname);
+}
+
 ReactDOM.render((
-  <Router history={browserHistory}>
+  <Router history={browserHistory} onUpdate={logPageView}>
     <Route path="/" component={Root}></Route>
   </Router>
 ),  document.getElementById('app'))
--- src/components/project-description.jsx
+++ src/components/project-description.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import ReactGa from 'react-ga';
 import {Link} from 'react-router';
 
 export default React.createClass({
   displayName : 'ProjectDescription',
+
+  track: function track(linkType) {
+    ReactGa.event({
+      category: 'User',
+      action: `User clicked ${linkType} link for ${this.props.title}`
+    });
+  },
+
   render: function render() {
     var liveLink = '';
     var sourceLink = '';
     var title = this.props.title;
     if (this.props.liveLink) {
-      liveLink = (<a href={this.props.liveLink}>live</a>);
+      liveLink = (
+        <a href={this.props.liveLink} onClick={this.track.bind(null, 'live')}>live</a>
+      );
     }
     if (this.props.sourceLink) {
-      sourceLink = (<a href={this.props.sourceLink}>source</a>);
+        sourceLink = (<a href={this.props.sourceLink} onClick={this.track.bind(null, 'source')}>source</a>
+      );
     }
     if (this.props.link) {
-      title = (<a href={this.props.link} className='title'>{this.props.title}</a>);
+      title = (
+        <a href={this.props.link} className='title' onClick={this.track.bind(null, 'main')}>{this.props.title}</a>);
     }
     return (
       <div className='project'>
