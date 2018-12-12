8ed4bc72df4188a15fdaab475d72aedaf333e430
Andrew McNutt <mcnutt.andrew@gmail.com>
2018-12-11 22:50:54 -0600

    add categories to projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,32 +3,52 @@ import React from 'react';
 import {PROJECTS} from '../constants';
 import GithubIcon from './github-icon';
 
+const SECTIONS = [
+  {name: 'Visualization', key: 'visualization'},
+  {name: 'Arts', key: 'art'},
+  {name: 'Tech & Other', key: 'tech'}
+].map(({name, key}) => ({
+  name,
+  projects: PROJECTS.filter(({section}) => section === key)
+}))
+
+const projectCard = (project, index) => {
+  const {
+    text,
+    dates,
+    link,
+    title,
+    imgLink,
+    sourceLink
+  } = project;
+  return (
+    <div className="project-block" key={index}>
+      <a className="pic-wrapper" href={link || sourceLink}>
+        <img className="project-image" src={imgLink}/>
+      </a>
+      <a className="project-title" href={link || sourceLink}>{title}</a>
+      {dates && <div className="project-dates">{dates}</div>}
+      <div className="project-links">
+        {sourceLink && <a href={sourceLink}>
+          <GithubIcon />
+        </a>}
+
+        {link && <a href={link}> LIVE </a>}
+      </div>
+      <div className="text-block">{text}</div>
+    </div>
+  );
+};
+
 const Projects = () => (
   <div className="page projects-page">
-    {PROJECTS.map((project, index) => {
-      const {
-        text,
-        dates,
-        link,
-        title,
-        imgLink,
-        sourceLink
-      } = project;
+    {SECTIONS.map(({projects, name}) => {
       return (
-        <div className="project-block" key={index}>
-          <a className="pic-wrapper" href={link || sourceLink}>
-            <img className="project-image" src={imgLink}/>
-          </a>
-          <a className="project-title" href={link || sourceLink}>{title}</a>
-          {dates && <div className="project-dates">{dates}</div>}
-          <div className="project-links">
-            {sourceLink && <a href={sourceLink}>
-              <GithubIcon />
-            </a>}
-
-            {link && <a href={link}> LIVE </a>}
+        <div key={name} className="project-page-subsection">
+          <h3>{name}</h3>
+          <div className="project-list">
+            {projects.map(projectCard)}
           </div>
-          <div className="text-block">{text}</div>
         </div>
       );
     })}
--- src/constants.js
+++ src/constants.js
@@ -74,6 +74,12 @@ export const PRESENTATIONS = [
 
 export const BLOG_POSTS = [
   {
+    title: 'A Brief Saga Concerning the Making of a Tarot Deck About the American Highway System',
+    subtitle: 'A little essay about making',
+    date: 'Personal Blog. December 10, 2018',
+    link: 'https://medium.com/@mcnutt/a-brief-saga-concerning-the-making-of-a-tarot-deck-about-the-american-highway-system-2eaebf3320c8'
+  },
+  {
     title: 'Advanced Visualization with react-vis',
     subtitle: 'Using Voronois, single pass rendering, and canvas components for amazing user experiences',
     date: 'Towards Data Science. May 21, 2018',
@@ -94,14 +100,16 @@ export const PROJECTS = [
     sourceLink: 'https://github.com/mcnuttandrew/flesch-kincaid-as-a-service',
     link: 'https://www.mcnutt.in/flesch-kincaid-as-a-service/',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/readability.png',
-    text: 'Have you ever wanted specific numerical quantification on how readable your prose is? This micro app wraps the textstat package as a webservice so that you can easily check.'
+    text: 'Have you ever wanted specific numerical quantification on how readable your prose is? This micro app wraps the textstat package as a webservice so that you can easily check.',
+    section: 'tech'
   },
   {
     title: 'tap-react-browser',
     dates: 'Feburary - April 2018',
     sourceLink: 'https://github.com/mcnuttandrew/tap-react-browser/',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/tap-react-browser.png',
-    text: 'In the process of building a variety of javascipt libraries in the coarse of my graduate research, I found myself needing a testing tool that played a particular role in relation to the browser, so I made one! tap-react-browser is a light wrapper on tape that spits out react components.'
+    text: 'In the process of building a variety of javascipt libraries in the coarse of my graduate research, I found myself needing a testing tool that played a particular role in relation to the browser, so I made one! tap-react-browser is a light wrapper on tape that spits out react components.',
+    section: 'tech'
   },
   {
     title: 'Constellations of Home - XMAS CARDS 2017',
@@ -109,7 +117,8 @@ export const PROJECTS = [
     link: 'http://www.mcnutt.in/home-graphs/',
     sourceLink: 'https://github.com/mcnuttandrew/home-graphs',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/home-graphs.png',
-    text: 'Over the 2017 holidays I spent some time meditating on memory, home, and graph theory, which led to my making these christmas cards.'
+    text: 'Over the 2017 holidays I spent some time meditating on memory, home, and graph theory, which led to my making these christmas cards.',
+    section: 'visualization'
   },
   {
     title: 'On The Shape of American Cities I/II',
@@ -117,7 +126,8 @@ export const PROJECTS = [
     link: 'http://www.mcnutt.in/city-size/',
     sourceLink: 'https://github.com/mcnuttandrew/city-size',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/city-size.png',
-    text: 'A print graphic describing the shape of the 100 most populous American cities.'
+    text: 'A print graphic describing the shape of the 100 most populous American cities.',
+    section: 'visualization'
   },
   {
     title: 'Pantone: Color of the year',
@@ -125,7 +135,8 @@ export const PROJECTS = [
     link: 'http://www.mcnutt.in/color-of-the-year/',
     sourceLink: 'https://github.com/mcnuttandrew/color-of-the-year',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/color-of-year.png',
-    text: 'A small exploration of the glory and wonder that is pantones color of the year.'
+    text: 'A small exploration of the glory and wonder that is pantones color of the year.',
+    section: 'visualization'
   },
   {
     title: 'react-vis',
@@ -133,7 +144,8 @@ export const PROJECTS = [
     link: 'http://uber.github.io/react-vis/#/',
     sourceLink: 'https://github.com/uber/react-vis',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/react-vis-image.png',
-    text: 'While working for Uber, I got a chance to act as lead maintainer of a charting library for the react ecosystem. It has a great API that enables really easy development of standard and non-standard chart types (eg check out my pantone project above). During my tenure in the role I added a wide variety of new chart types, added hundreds of tests, developed a canvas rendering system, and just generally gave it lots of love. I still act as a maintainer, but I am no longer the lead.'
+    text: 'A charting library for the react ecosystem.',
+    section: 'visualization'
   },
   {
     title: 'CSV Conversion',
@@ -141,7 +153,8 @@ export const PROJECTS = [
     link: 'http://www.mcnutt.in/csv-conversion/',
     sourceLink: 'https://github.com/mcnuttandrew/csv-conversion',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/csv-conversion.png',
-    text: 'A handy client-side csv to json converter. I built this little app, because my favorite conversion site got knocked down and I wanted to improve the UI.'
+    text: 'A handy client-side csv to json converter. I built this little app, because my favorite conversion site got knocked down and I wanted to improve the UI.',
+    section: 'tech'
   },
   {
     title: 'Personal Timeline',
@@ -149,7 +162,17 @@ export const PROJECTS = [
     link: 'http://www.mcnutt.in/personal-timeline/',
     sourceLink: 'https://github.com/mcnuttandrew/personal-timeline',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/personal-time.png',
-    text: 'A brief timeline of my life, a resume through a dark mirror if you will.'
+    text: 'A brief timeline of my life, a resume through a dark mirror if you will.',
+    section: 'visualization'
+  },
+  {
+    title: 'Unnamed Tarot Deck',
+    dates: 'June 2016',
+    link: 'https://medium.com/@mcnutt/a-brief-saga-concerning-the-making-of-a-tarot-deck-about-the-american-highway-system-2eaebf3320c8',
+    sourceLink: 'https://github.com/mcnuttandrew/tarot-deck',
+    imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/tarot-image.png',
+    text: 'A tarot tech themed around the signage and spirit of the American highway system.',
+    section: 'art'
   },
   {
     title: 'Why Not Ipsum',
@@ -157,14 +180,17 @@ export const PROJECTS = [
     link: 'http://why-not-ipsum.herokuapp.com/',
     sourceLink: 'https://github.com/mcnuttandrew/Why-Not-Zoidberg',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/why-not-image.png',
-    text: 'A Lorem Ipsum generator populated by Zoidberg quotes, built following RESTful design practices. Included a large series of data scrapes, which were necessarily followed by intensive data cleaning.'
+    text: 'A Lorem Ipsum generator populated by Zoidberg quotes, built following RESTful design practices. Included a large series of data scrapes, which were necessarily followed by intensive data cleaning.',
+    section: 'tech'
   },
   {
     title: 'N-Hedron',
     dates: 'September - December 2013',
     imgLink: 'https://s3-us-west-1.amazonaws.com/mcnutt-static-images/n-hydron.png',
     link: '../assets/nhedron.pdf',
-    text: 'Throughout my undergraduate career I had a prevailing interest in the geometrically ambiguous shape, the N-Hedron. It is formed by taking an integer, N, number of points, placing them on the sphere and demanding that they be maximally far apart. This culminated in a independent project, in which I implemented three different algorithms for constructing these shapes. You can find out more about this project. One of the major aspects of this project was dealing with a large (for Mathematica) amount of data which reached up into the millions of data points.'
+    text: 'An independent college project regarding the effacy of various numerical algorithms for constructing the n-hedron.',
+    // text: 'Throughout my undergraduate career I had a prevailing interest in the geometrically ambiguous shape, the N-Hedron. It is formed by taking an integer, N, number of points, placing them on the sphere and demanding that they be maximally far apart. This culminated in a independent project, in which I implemented three different algorithms for constructing these shapes. You can find out more about this project. One of the major aspects of this project was dealing with a large (for Mathematica) amount of data which reached up into the millions of data points.',
+    section: 'tech'
   }
 
   // {
