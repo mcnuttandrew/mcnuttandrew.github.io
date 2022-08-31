9c1040032b957c442ded183ef64a83b7aea594ee
mcnuttandrew <mcnutt.andrew@gmail.com>
2022-08-31 14:37:20 +0000

    Deploying to gh-pages from @ mcnuttandrew/mcnuttandrew.github.io@6edcd5023da2f7cb6446466d79930e126accf14f 🚀

--- src/constants.ts
+++ src/constants.ts
@@ -45,7 +45,8 @@ type linkType =
   | 'slides'
   | 'blog post'
   | 'award'
-  | 'thesis';
+  | 'thesis'
+  | 'replicability award';
 export interface Publication {
   link?: string;
   urlTitle: string;
@@ -74,7 +75,11 @@ export const PUBLICATIONS: Publication[] = [
     date: 'October 2022',
     links: [
       {name: 'paper', link: 'https://arxiv.org/pdf/2207.07998.pdf'},
-      {name: 'live', link: 'https://vis-json-dsls.netlify.app/'}
+      {name: 'live', link: 'https://vis-json-dsls.netlify.app/'},
+      {
+        name: 'replicability award',
+        link: 'http://www.replicabilitystamp.org/#https-github-com-mcnuttandrew-no-grammar-supplement'
+      }
     ],
     abstract:
       'There has been substantial growth in the use of JSON-based grammars, as well as other standard data serialization languages, to create visualizations. Each of these grammars serves a purpose: some focus on particular computational tasks (such as animation), some are concerned with certain chart types (such as maps), and some target specific data domains (such as ML). Despite the prominence of this interface form, there has been little detailed analysis of the characteristics of these languages.  In this study, we survey and analyze the design and implementation of 57 JSON-style DSLs for visualization. We analyze these languages supported by a collected corpus of examples for each DSL (consisting of 4395 instances) across a variety of axes organized into concerns related to domain, conceptual model, language relationships, affordances, and general practicalities. We identify tensions throughout these areas, such as between formal and colloquial specifications, among types of users, and within the composition of languages. Through this work, we seek to support language implementers by elucidating the choices, opportunities, and tradeoffs in visualization DSL design.',
