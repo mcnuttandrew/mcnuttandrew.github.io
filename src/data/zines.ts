c72794ea90c5536902a097a2cb1f4bc4566d8344
mcnuttandrew <mcnutt.andrew@gmail.com>
2022-10-08 23:53:06 +0000

    Deploying to gh-pages from @ mcnuttandrew/mcnuttandrew.github.io@96cee745a3d374eda662e8a353d87efe89ae5003 🚀

--- src/data/zines.ts
+++ src/data/zines.ts
@@ -16,27 +16,27 @@ const ZINES: Zine[] = [
 
   {
     title: '"How to read an academic paper" zine',
-    link: 'http://www.mcnutt.in/paper-zine/',
-    imgLink: 'http://www.mcnutt.in/paper-zine/cover.png',
+    link: 'https://www.mcnutt.in/paper-zine/',
+    imgLink: 'https://www.mcnutt.in/paper-zine/cover.png',
     text: 'A non-definitive guide to paper reading.',
     year: 2021
   },
   {
     title: 'On the potential of zines as a medium for visualization',
     link: 'https://www.mcnutt.in/zine-potential/',
-    imgLink: 'http://www.mcnutt.in/converted-images/zine-potential.jpg',
+    imgLink: 'https://www.mcnutt.in/converted-images/zine-potential.jpg',
     text: 'A zine about my paper about the potential of zines for data visualization.',
     year: 2021
   },
   {
     title: 'Bison Elysium National Park',
-    imgLink: 'http://www.mcnutt.in/converted-images/bison-eylsium-cover.jpg',
+    imgLink: 'https://www.mcnutt.in/converted-images/bison-eylsium-cover.jpg',
     text: 'A brochure for a fictional national park made using MidJourney and GPT3. (print only)',
     year: 2022
   },
   {
     title: 'No Grammar to Rule Them All',
-    imgLink: 'http://www.mcnutt.in/converted-images/no-grammar-zine-cover.jpg',
+    imgLink: 'https://www.mcnutt.in/converted-images/no-grammar-zine-cover.jpg',
     text: 'A zine for my paper "No Grammar to rule them all: a survey of JSON-style DSLs for Visualization". (print only)',
     year: 2022
   }
