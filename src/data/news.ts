23b9801af1d2bdbad54ef8dc950ea6c32ece3052
mcnuttandrew <mcnutt.andrew@gmail.com>
2024-05-28 15:33:00 +0000

    Deploying to gh-pages from @ mcnuttandrew/mcnuttandrew.github.io@e40e79609f6d218cd3e77441ecf00f9521a45c81 🚀

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,180 +1,182 @@
 export const NEWS = [
   {
-    date: 'April 2024',
+    date: "April 2024",
     content:
-      "Excited to be co-organizing [VISions of the Future](https://visionsofthefuture.github.io/') an VIS24 workshop on cultural📈 and environmental🌴 sustainability. "
+      "Excited to be co-organizing [VISions of the Future](https://visionsofthefuture.github.io/) an VIS24 workshop on cultural📈 and environmental🌴 sustainability. ",
   },
   {
-    date: 'September 2023',
+    date: "September 2023",
     content:
-      "Started my PostDoc at University of Washington's [Interactive Data Lab](https://idl.cs.washington.edu/) with [Leilani Battle](https://homes.cs.washington.edu/~leibatt/bio.html) and [Jeffrey Heer](https://homes.cs.washington.edu/~jheer/)."
+      "Started my PostDoc at University of Washington's [Interactive Data Lab](https://idl.cs.washington.edu/) with [Leilani Battle](https://homes.cs.washington.edu/~leibatt/bio.html) and [Jeffrey Heer](https://homes.cs.washington.edu/~jheer/).",
   },
   {
-    date: 'August 2023',
+    date: "August 2023",
     content:
-      'Our full paper on [evaluating visualization notations](https://arxiv.org/pdf/2308.16353.pdf) 📊 will be presented at VIS 2023! Also excited that [our reflection](https://arxiv.org/abs/2308.15429) on the 🌲🌲🌲environmental sustainability🌲🌲🌲 of IEEE VIS will be presented at alt.vis 2023!!'
+      "Our full paper on [evaluating visualization notations](https://arxiv.org/pdf/2308.16353.pdf) 📊 will be presented at VIS 2023! Also excited that [our reflection](https://arxiv.org/abs/2308.15429) on the 🌲🌲🌲environmental sustainability🌲🌲🌲 of IEEE VIS will be presented at alt.vis 2023!!",
   },
   {
-    date: 'June 2023',
+    date: "June 2023",
     content:
-      'I got my Ph.D.!! Thanks to my committee, mentors, friends, family, and everyone else who supported me through it.'
+      "I got my Ph.D.!! Thanks to my committee, mentors, friends, family, and everyone else who supported me through it.",
   },
 
   {
-    date: 'April 2023',
-    content: '[Our paper](https://osf.io/3jrcm/) on generative AI methods was accepted to EuroVIS23! '
-  },
-  {
-    date: 'March 2023',
+    date: "April 2023",
     content:
-      'At long last I have been awarded the College Teaching Certificate from the University of Chicago Center for Teaching.'
+      "[Our paper](https://osf.io/3jrcm/) on generative AI methods was accepted to EuroVIS23! ",
   },
   {
-    date: 'January 2023',
+    date: "March 2023",
     content:
-      'Thrilled to have two (first-author) papers accepted to CHI 2023!! One came from my internship with [MSR last summer](https://arxiv.org/abs/2301.11178), while the other is the culmination of a years long work on [looking at creative coding editors](https://people.cs.uchicago.edu/~rchugh/classes/creative-coding/).'
+      "At long last I have been awarded the College Teaching Certificate from the University of Chicago Center for Teaching.",
   },
   {
-    date: 'September 2022',
+    date: "January 2023",
     content:
-      'Honored to be named to [Siebel Scholars](https://cs.uchicago.edu/news/five-uchicago-cs-students-named-to-siebel-scholars-2023-class/) class of 2023!'
+      "Thrilled to have two (first-author) papers accepted to CHI 2023!! One came from my internship with [MSR last summer](https://arxiv.org/abs/2301.11178), while the other is the culmination of a years long work on [looking at creative coding editors](https://people.cs.uchicago.edu/~rchugh/classes/creative-coding/).",
   },
   {
-    date: 'July 2022',
+    date: "September 2022",
     content:
-      '[Our paper](https://link.springer.com/article/10.1007/s12064-022-00376-8) on the role of nationalism in 🔬science🔬 in 19th century France was (at long last) published Theory in Biosciences!!'
+      "Honored to be named to [Siebel Scholars](https://cs.uchicago.edu/news/five-uchicago-cs-students-named-to-siebel-scholars-2023-class/) class of 2023!",
   },
   {
-    date: 'July 2022',
+    date: "July 2022",
     content:
-      '[My paper](https://arxiv.org/pdf/2207.07998.pdf) exploring the terrain of JSON-style DSLs for visualization was accepted to VIS22.'
+      "[Our paper](https://link.springer.com/article/10.1007/s12064-022-00376-8) on the role of nationalism in 🔬science🔬 in 19th century France was (at long last) published Theory in Biosciences!!",
   },
   {
-    date: 'May 2022',
-    content: 'Started a (remote) internship at Microsoft Research with Chenglong Wang and Rob DeLine.'
-  },
-  {
-    date: 'April 2022',
+    date: "July 2022",
     content:
-      'Our paper ([Explaining Why](https://www.blaseur.com/papers/rationales-naacl22.pdf)) on interfaces for providing annotator rationales was accepted to NAACL 2022!'
+      "[My paper](https://arxiv.org/pdf/2207.07998.pdf) exploring the terrain of JSON-style DSLs for visualization was accepted to VIS22.",
   },
   {
-    date: 'March 2022',
+    date: "May 2022",
     content:
-      'Quite excited to be serving on the organizing committee for [alt.vis](https://altvis.github.io/) this year!'
+      "Started a (remote) internship at Microsoft Research with Chenglong Wang and Rob DeLine.",
   },
   {
-    date: 'February 2022',
+    date: "April 2022",
     content:
-      'Delighted to now be a PhD Candidate! Tremendous thanks to my committee Ravi Chugh, Blase Ur, and Arvind Satyanarayan.'
+      "Our paper ([Explaining Why](https://www.blaseur.com/papers/rationales-naacl22.pdf)) on interfaces for providing annotator rationales was accepted to NAACL 2022!",
   },
   {
-    date: 'October 2021',
+    date: "March 2022",
     content:
-      'Excited that [our paper](https://arxiv.org/abs/2109.06007) describing the design space of visualization applications for villains was accepted to [alt.vis 21](https://altvis.github.io/).'
+      "Quite excited to be serving on the organizing committee for [alt.vis](https://altvis.github.io/) this year!",
   },
   {
-    date: 'August 2021',
+    date: "February 2022",
     content:
-      'Thanks to [RAMPVIS](https://sites.google.com/view/rampvis/events?authuser=0) for hosting me! I had a great time talking with them about all things algebraic analysis. [Check out the slides](https://drive.google.com/file/d/1o9-7cBnQXfO8VTMS9V7MZ_5odwYVUkYw/view?usp=sharing)'
+      "Delighted to now be a PhD Candidate! Tremendous thanks to my committee Ravi Chugh, Blase Ur, and Arvind Satyanarayan.",
   },
   {
-    date: 'August 2021',
+    date: "October 2021",
     content:
-      'My paper ["On The Potential of Zines as Medium for Visualization"](https://www.mcnutt.in/zine-potential) was accepted to the VIS21 short papers track! (The paper includes a zine about the paper)'
+      "Excited that [our paper](https://arxiv.org/abs/2109.06007) describing the design space of visualization applications for villains was accepted to [alt.vis 21](https://altvis.github.io/).",
   },
   {
-    date: 'July 2021',
+    date: "August 2021",
     content:
-      'Our paper on [KondoCloud](https://dl.acm.org/doi/10.1145/3472749.3474736), a system that asks users if files in their cloud-based drives spark joy, was accepted to UIST21!'
+      "Thanks to [RAMPVIS](https://sites.google.com/view/rampvis/events?authuser=0) for hosting me! I had a great time talking with them about all things algebraic analysis. [Check out the slides](https://drive.google.com/file/d/1o9-7cBnQXfO8VTMS9V7MZ_5odwYVUkYw/view?usp=sharing)",
   },
   {
-    date: 'June 2021',
+    date: "August 2021",
     content:
-      'My paper on [table cartograms and algebraic visualization](https://www.mcnutt.in/#/research/tacos) won the honorable mention🏆 for best paper at EuroVis!'
+      'My paper ["On The Potential of Zines as Medium for Visualization"](https://www.mcnutt.in/zine-potential) was accepted to the VIS21 short papers track! (The paper includes a zine about the paper)',
   },
   {
-    date: 'March 2021',
+    date: "July 2021",
     content:
-      'My paper ["What are Table Cartograms Good for Anyway? An Algebraic Analysis"](https://www.mcnutt.in/#/research/tacos) was accepted to EuroVIS 2021! 🌮🌮🌮🌮'
+      "Our paper on [KondoCloud](https://dl.acm.org/doi/10.1145/3472749.3474736), a system that asks users if files in their cloud-based drives spark joy, was accepted to UIST21!",
   },
   {
-    date: 'January 2021',
+    date: "June 2021",
     content:
-      'Our paper 🌱["Integrated Visualization Editing via Parameterized Declarative Templates"](https://www.mcnutt.in/#/research/ivy) was accepted to CHI 2021.'
+      "My paper on [table cartograms and algebraic visualization](https://www.mcnutt.in/#/research/tacos) won the honorable mention🏆 for best paper at EuroVis!",
   },
   {
-    date: 'October 2020',
+    date: "March 2021",
     content:
-      '["A Minimally Constrained Optimization Algorithm for Table Cartograms"](https://osf.io/kem6j/) won an honorable mention🏆 in the IEEEVIS InfoVis poster track.'
+      'My paper ["What are Table Cartograms Good for Anyway? An Algebraic Analysis"](https://www.mcnutt.in/#/research/tacos) was accepted to EuroVIS 2021! 🌮🌮🌮🌮',
   },
   {
-    date: 'September 2020',
+    date: "January 2021",
     content:
-      'Our paper ["Supporting Expert Close Analysis of Historical Scientific Writings: A Case Study for Near-by Reading"](https://arxiv.org/pdf/2009.02384.pdf) on mixing close and distant reading for 19th century scientific writing was accepted to [VIS4DH20](http://www.vis4dh.org/).'
+      'Our paper 🌱["Integrated Visualization Editing via Parameterized Declarative Templates"](https://www.mcnutt.in/#/research/ivy) was accepted to CHI 2021.',
   },
   {
-    date: 'March 2020',
+    date: "October 2020",
     content:
-      'Our paper ["Surfacing Visualization Mirages"](https://www.mcnutt.in/#/research/mirage) won a best paper honorable mention🏆 at CHI2020!!'
+      '["A Minimally Constrained Optimization Algorithm for Table Cartograms"](https://osf.io/kem6j/) won an honorable mention🏆 in the IEEEVIS InfoVis poster track.',
   },
   {
-    date: 'February 2020',
+    date: "September 2020",
     content:
-      'Our paper on using tarot for visual analytics ["Divining Insights: Visual Analytics Through Cartomancy"](https://www.mcnutt.in/#/research/tarot) was accepted to alt.chi 2020'
+      'Our paper ["Supporting Expert Close Analysis of Historical Scientific Writings: A Case Study for Near-by Reading"](https://arxiv.org/pdf/2009.02384.pdf) on mixing close and distant reading for 19th century scientific writing was accepted to [VIS4DH20](http://www.vis4dh.org/).',
   },
   {
-    date: 'January 2020',
+    date: "March 2020",
     content:
-      'Started teaching my first class as the instructor of record! (["Data Visualization for Public Policy"](https://capp-30239-winter-2020.netlify.app/))'
+      'Our paper ["Surfacing Visualization Mirages"](https://www.mcnutt.in/#/research/mirage) won a best paper honorable mention🏆 at CHI2020!!',
   },
   {
-    date: 'December 2019',
-    content: 'Received my Masters of Computer Science!!'
-  },
-  {
-    date: 'November 2019',
+    date: "February 2020",
     content:
-      'Our poster on using graph visualizations to compare 19th century scientific writing was shown at both MindBytes (where it won best poster!) and the 2019 Chicago Colloquium on Digital Humanities.'
+      'Our paper on using tarot for visual analytics ["Divining Insights: Visual Analytics Through Cartomancy"](https://www.mcnutt.in/#/research/tarot) was accepted to alt.chi 2020',
   },
   {
-    date: 'October 2019',
+    date: "January 2020",
     content:
-      'Had a great time at IEEEVIS 2019 in Vancouver, BC! Helped contribute to the [Open Access Vis](http://oavis.org/) efforts for the conference.'
+      'Started teaching my first class as the instructor of record! (["Data Visualization for Public Policy"](https://capp-30239-winter-2020.netlify.app/))',
   },
   {
-    date: 'September 2019',
+    date: "December 2019",
+    content: "Received my Masters of Computer Science!!",
+  },
+  {
+    date: "November 2019",
     content:
-      'Two of my vis projects (ForumExplorer and Cycles in Rain) were Long-Listed at the Kantar Information is Beautiful Awards.'
+      "Our poster on using graph visualizations to compare 19th century scientific writing was shown at both MindBytes (where it won best poster!) and the 2019 Chicago Colloquium on Digital Humanities.",
   },
   {
-    date: 'June 2019',
-    content: 'Started an internship with Tableau Research in Seattle'
-  },
-  {
-    date: 'June 2019',
-    content: '🎉🎉🎉 Successfully defended my masters thesis 🎉🎉🎉'
-  },
-  {
-    date: 'May 2019',
+    date: "October 2019",
     content:
-      'Presented our [poster on ForumExplorer](https://www.mcnutt.in/forum-explorer/) at EuroVis 2019 in Porto, Portugal.'
+      "Had a great time at IEEEVIS 2019 in Vancouver, BC! Helped contribute to the [Open Access Vis](http://oavis.org/) efforts for the conference.",
   },
   {
-    date: 'May 2019',
+    date: "September 2019",
     content:
-      'I was awarded the UChicago Department of Computer Science TA Prize for my work TAing Spring 2018 - Winter 2019.'
+      "Two of my vis projects (ForumExplorer and Cycles in Rain) were Long-Listed at the Kantar Information is Beautiful Awards.",
   },
   {
-    date: 'May 2019',
-    content:
-      "My piece 'On The Road To The Lake: Debugging in Tryptic' won second prize in the print media category of the UChicago Art+Science expo."
+    date: "June 2019",
+    content: "Started an internship with Tableau Research in Seattle",
   },
   {
-    date: 'October 2018',
+    date: "June 2019",
+    content: "🎉🎉🎉 Successfully defended my masters thesis 🎉🎉🎉",
+  },
+  {
+    date: "May 2019",
     content:
-      'Presented our paper on linting charts created in matplotlib at VisGuides, a IEEEVIS 2018 workshop. '
-  }
+      "Presented our [poster on ForumExplorer](https://www.mcnutt.in/forum-explorer/) at EuroVis 2019 in Porto, Portugal.",
+  },
+  {
+    date: "May 2019",
+    content:
+      "I was awarded the UChicago Department of Computer Science TA Prize for my work TAing Spring 2018 - Winter 2019.",
+  },
+  {
+    date: "May 2019",
+    content:
+      "My piece 'On The Road To The Lake: Debugging in Tryptic' won second prize in the print media category of the UChicago Art+Science expo.",
+  },
+  {
+    date: "October 2018",
+    content:
+      "Presented our paper on linting charts created in matplotlib at VisGuides, a IEEEVIS 2018 workshop. ",
+  },
   // {
   //   date: 'September 2018',
   //   content: 'Started my second year of grad school!'
