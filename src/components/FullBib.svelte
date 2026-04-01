62becc6ef45412954b6d49bb1a47eb16735f7153
Andrew McNutt <mcnutt.andrew@gmail.com>
2026-04-01 15:39:48 -0600

    Add a bib page, add dois to everything

---
+++ src/components/FullBib.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { PUBLICATIONS } from "../data/publications";
+  import { buildBibTexEntry } from "../utils";
+  import markdownit from "markdown-it";
+  const md = markdownit({
+    html: true,
+    linkify: true,
+    typographer: true,
+  });
+</script>
+
+<div class="p-4">
+  <div class="border bg-stone-300 p-4">
+    <code class="">
+      {#each PUBLICATIONS as publication}
+        {@html md.render(buildBibTexEntry(publication))}
+      {/each}
+    </code>
+  </div>
+</div>
