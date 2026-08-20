3eaa31c6108e1127ddbc73dbf0656a4240ac5b55
Andrew McNutt <mcnutt.andrew@gmail.com>
2026-08-20 09:54:37 -0400

    Color pal poster

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -156,9 +156,9 @@ export const PUBLICATIONS: Publication[] = [
   },
 
   {
-    link: "",
+    link: "https://arxiv.org/abs/2608.19148",
     title: "Trade-offs in Data Color Palette Design Tools",
-    doi: "TBD",
+    doi: "NA",
     authors: "Shiyi He, Andrew McNutt",
     year: 2026,
     journal: "IEEE VIS Posters",
@@ -167,7 +167,16 @@ export const PUBLICATIONS: Publication[] = [
     imgDescription: "A set of stepped gradients",
     type: "paper",
     topics: ["Color"],
-    links: [],
+    links: [
+      {
+        name: "paper",
+        link: "https://arxiv.org/abs/2608.19148",
+      },
+      {
+        name: "study",
+        link: "https://shiyihe-neko.github.io/reVISit_study/color-buddy/",
+      },
+    ],
     abstract: `Designing a color palette for data requires designers to balance multiple constraints, including accessibility and aesthetics. Color palette tools support this process through features icluding direct manipulation, automated palette generation and evaluation, previews, and so on. Despite their prominence, relatively little is known about how these different mechanisms shape design across contexts. We conducted an exploratory think-aloud crowd work study with 40 self-identified designers. Each participant used one of four palette tools selected to span different interaction modalities to complete a series of accessibility- and aesthetics-oriented design tasks. We observed two preliminary patterns. First, tool differences were more pronounced in accessibility-constrained tasks. Second, even when accessibility was not explicitly required, some tools produced more accessibility-friendly palettes and prompted more accessibility-oriented thinking. In this tool genre, then, system design shapes outcomes both via built-in functionality, as well as by directing designers' attention toward particular constraints and design considerations.`,
   },
 
